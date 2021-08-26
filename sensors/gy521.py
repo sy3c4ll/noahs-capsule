91351249fbc0d4e0dcfbcdedb3015d9d2e993f0f
sy3c4ll <sy3c4ll@gmail.com>
2021-08-26 22:20:05 +0900

    fix gy521.py

--- sensors/gy521.py
+++ sensors/gy521.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 import smbus
 import sys
+import time
 if len(sys.argv)!=2:
   raise OSError('Invalid number of arguments')
 bus=smbus.SMBus(1)
 bus.write_byte_data(0x68,0x6b,0)
 def read(reg):
+  time.sleep(0.2)
   val=(bus.read_byte_data(0x68,reg)<<8)+bus.read_byte_data(0x68,reg+1)
   if val>=0x8000:
     return val-0x10000
   else:
     return val
 with open(sys.argv[1],'wt') as f:
-  f.write('{"gx":'+read(0x43)/0x83+',"gy":'+read(0x45)/0x83+',"gz":'+read(0x47)/0x83+',"ax":'+read(0x3b)/0x8000+',"ay":'+read(0x3d)/0x8000+',"az":'+read(0x3f)/0x8000+'}')
+  f.write('{"gx":'+str(read(0x43))+',"gy":'+str(read(0x45))+',"gz":'+str(read(0x47))+',"ax":'+str(read(0x3b))+',"ay":'+str(read(0x3d))+',"az":'+str(read(0x3f))+'}')
 
